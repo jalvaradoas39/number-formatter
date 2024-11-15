c710ecac11d393ae698d9999421c116540efa6c3
Jorge Alvarado <jalvaradoas39@outlook.com>
2024-11-15 17:56:14 -0500

    init commit

--- src/NumberFormatter.java
+++ src/NumberFormatter.java
@@ -1,6 +1,5 @@
 /**
- * Description: This interface provides a method for formatting an integer.
- * It is intended to be implemented by different classes providing specific formatting styles.
+ * Description: This interface provides a method for formatting an integer. It is intended to be implemented by different classes providing specific formatting styles.
  */
 public interface NumberFormatter {
     /**
